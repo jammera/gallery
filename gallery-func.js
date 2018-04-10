e2c75594301bec08791ff9abf722ee20cb2f4aa3
jammera <kamen.boev@gmail.com>
2018-04-11 01:26:41 +0300

    Update gallery-func.js

--- gallery-func.js
+++ gallery-func.js
@@ -9,3 +9,26 @@ const viewSelector = () => {
 const makeCollage = () => {
 	alert("make collage...")
 }
+
+// Get the modal
+let modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+let img = '';
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+const getId = (id) => {
+	img = document.getElementById(id);
+	modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+    console.log(modal.style.display, modalImg, captionText.innerHTML)
+	console.log(id);
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
